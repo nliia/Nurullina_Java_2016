2d2b965a0c87fe9628d616e30b823d5a588dfac1
nliia <linu28@mail.ru>
2016-11-16 23:38:19 +0300

    AY-2: corrected

---
+++ Hw1_multimod/main/src/main/java/com/nurullina/Main.java
@@ -0,0 +1,15 @@
+package com.nurullina;
+
+
+import static com.nurullina.MathMethods.divide;
+import static com.nurullina.ReverseSum.reverseSum;
+
+/**
+ * Created by Liia on 15.11.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Reversed sum of numbers:" + reverseSum(15, 25));
+        System.out.println("Division:" + divide(30, 15));
+    }
+}
---
+++ Hw1_multimod/reverse/src/main/java/com/nurullina/ReverseSum.java
@@ -0,0 +1,21 @@
+package com.nurullina;
+
+
+import static com.nurullina.MathMethods.sum;
+
+/**
+ * Created by Liia on 15.11.2016.
+ */
+public class ReverseSum {
+
+    public static String reverseSum(int a, int b) {
+        int c = sum(a, b);
+        StringBuilder reverseNumber = new StringBuilder();
+        while (c > 9) {
+            reverseNumber.append(c % 10);
+            c = c / 10;
+        }
+
+        return reverseNumber.append(c).toString();
+    }
+}
